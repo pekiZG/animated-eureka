28cecc44e24dca758031cc9f00299ca8efaa7dbb
Petar Šlat <petar.slat@gmail.com>
2018-04-12 22:18:33 +0200

    Solve 3.2.6

--- src/hr/java/vjezbe/entitet/SenzorTemperature.java
+++ src/hr/java/vjezbe/entitet/SenzorTemperature.java
@@ -1,5 +1,11 @@
 package hr.java.vjezbe.entitet;
 
+import java.math.BigDecimal;
+import java.util.Random;
+
+import hr.java.vjezbe.iznimke.NiskaTemperaturaException;
+import hr.java.vjezbe.iznimke.VisokaTemperaturaException;
+
 public class SenzorTemperature extends Senzor {
 
 	private String nazivElektronickeKomponente;
@@ -13,6 +19,18 @@ public class SenzorTemperature extends Senzor {
 	public String dohvatiPodatkeSenzora() {
 		return "Komponenta: " + this.nazivElektronickeKomponente + ", vrijednost: " + super.getVrijednost() + " "
 				+ super.getMjernaJedinica();
+	}
+	
+	public void generirajVrijednost() throws VisokaTemperaturaException {
+		Integer randomValue = -50 + new Random().nextInt(100);
+		BigDecimal nasumicniBroj = new BigDecimal(randomValue);
+		super.setVrijednost(nasumicniBroj);
+		if (randomValue > 40) {
+			throw new VisokaTemperaturaException();
+		}
+		if (randomValue < -10) {
+			throw new NiskaTemperaturaException();
+		}
 	}
 
 }
