12b7454729f95afec7c7a18f6bf241925fb475e7
Petar Šlat <petar.slat@gmail.com>
2018-04-12 22:46:15 +0200

    Solve 3.2.12

--- src/hr/java/vjezbe/iznimke/VisokaTemperaturaException.java
+++ src/hr/java/vjezbe/iznimke/VisokaTemperaturaException.java
@@ -7,17 +7,33 @@ public class VisokaTemperaturaException extends Exception {
 	 */
 	private static final long serialVersionUID = -2464415359284430992L;
 
+	/**
+	 * Baca Visoka Temperatura exception
+	 * @throws Exception
+	 */
 	public VisokaTemperaturaException() {
 	}
 
+	/**
+	 * Propagira poruku greška
+	 * @param message
+	 * @author Petar
+	 */
 	public VisokaTemperaturaException(String message) {
 		super(message);
 	}
 
+	/**
+	 * @param cause
+	 */
 	public VisokaTemperaturaException(Throwable cause) {
 		super(cause);
 	}
 
+	/**
+	 * @param message
+	 * @param cause
+	 */
 	public VisokaTemperaturaException(String message, Throwable cause) {
 		super(message, cause);
 	}
